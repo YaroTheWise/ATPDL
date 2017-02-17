af91e5f4da0e54d42e94dedd76544fc2f3ba399b
Shein Yaroslav <sheinyaroslav@gmail.com>
2017-02-18 00:28:03 +0500

    Добавил главный проект

---
+++ ATPDL.Main/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ATPDL.Main
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var client = new DataLoader.DataLoader();
+            client.LoadPlayers().GetAwaiter().GetResult();
+        }
+    }
+}
